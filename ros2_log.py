d3916035f4f32a38ae7c67586e75af69696f193a
balao1312 <balao1014@hotmail.com>
2021-04-29 14:11:09 +0800

    send line

--- ros2_log.py
+++ ros2_log.py
@@ -8,6 +8,7 @@ import re
 import sys
 import time
 import os
+import requests
 from copy import copy
 from config import config
 
@@ -18,11 +19,36 @@ class ros2_logger(Amari_logger):
     interval = 1
     buffer = datetime.timedelta(seconds=1)
 
+    last_value = {}
+    token = 'xrAUKB7KDmFh0CC97D1hgMl7NDNRimXK9GDF7SJOTFw'
+
     def __init__(self):
         super().__init__()
 
         self.log_file = self.log_folder.joinpath(
             f'log_ros2_{datetime.datetime.now().date()}')
+
+    def lineNotifyMessage(line_token, msg):
+        line_headers = {
+            "Authorization": "Bearer " + line_token,
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+
+        payload = {'message': msg}
+        r = requests.post("https://notify-api.line.me/api/notify",
+                          headers=line_headers, params=payload)
+        return r.status_code
+
+    def check_if_status_changed(self, tag, value):
+        if tag not in self.last_value:
+            return
+            
+        if not value == self.last_value[tag]:
+            try:
+                msg = f'\n{tag} status change detected:\n{datetime.datetime.now()}\nbefore: {self.last_value[tag]}\nnow: {value}'
+                self.lineNotifyMessage(self.token, msg)
+            except Exception as e:
+                print(f'send line notify error: {e.__class__}, \n{e}')
 
     def parse_and_send_data(self, dict, tag):
         keys_to_delete = []
@@ -32,6 +58,9 @@ class ros2_logger(Amari_logger):
             if datetime.datetime.utcnow() - time_object > self.buffer:
                 value = sorted(value)
                 print(f'{time_string}, {tag} alive:', value)
+
+                self.check_if_status_changed(tag, value)
+                self.last_value[tag] = value
 
                 # total alive count
                 data = {
