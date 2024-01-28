8eb61732cf81004f9370cdff02c77c81da2e2ad4
balao1312 <balao1014@hotmail.com>
2024-01-28 10:36:43 +0800

    Feat: iperf_server_logger auto detect parallel thread numbers to get total tput.

--- iperf3_server_logger.py
+++ iperf3_server_logger.py
@@ -14,10 +14,9 @@ import signal
 
 class Iperf3_logger(Amari_logger):
 
-    def __init__(self, port, parallel, notify_when_terminated, label, dont_send_to_db, project_field_name, notify_dst):
+    def __init__(self, port, notify_when_terminated, label, dont_send_to_db, project_field_name, notify_dst):
         super().__init__()
         self.port = port
-        self.is_in_parallel_mode = parallel
         self.notify_when_terminated = notify_when_terminated
         self.label = label
         self.project_field_name = project_field_name
@@ -49,20 +48,19 @@ class Iperf3_logger(Amari_logger):
         print(self.turn_to_form('project field name', self.project_field_name))
         print(self.turn_to_form('notify when terminated',
               str(bool(self.notify_when_terminated))))
-        print(self.turn_to_form('parallel mode',
-              str(bool(self.is_in_parallel_mode))))
         print(self.turn_to_form('notify destination', self.notify_dst))
-        print('==> !!!!!!!!!!!!!!!!! check parallel mode arg or the result would be wrong!')
         print(f'==> original cmd send: \n\n\t{self.cmd}\n')
-    
-    def validate_notify_dst(self): 
+
+    def validate_notify_dst(self):
         if self.can_send_line_notify:
             if not self.notify_dst:
-                print(f'==> Waring: Notify destination is not set, notify function is disabled.')
+                print(
+                    f'==> Waring: Notify destination is not set, notify function is disabled.')
                 self.can_send_line_notify = False
                 return
             if not self.notify_dst in self.line_notify_dsts:
-                print(f'\n==> Notify destination "{self.notify_dst}" is not valid, notify function is disabled.\n')
+                print(
+                    f'\n==> Notify destination "{self.notify_dst}" is not valid, notify function is disabled.\n')
                 self.can_send_line_notify = False
                 return
 
@@ -105,12 +103,15 @@ class Iperf3_logger(Amari_logger):
             'msg': msg
         }
 
-    def notify_when_client_termainated_on_demand(self, line):
+    def validate_to_send_notify(self, msg):
         if not self.can_send_line_notify:
             return
+        self.unsend_line_notify_queue.put(self.gen_notify_format(msg))
+
+    def check_if_client_is_terminated_and_send_notify_on_demand(self, line):
         if self.terminated_pattern.match(line) and self.notify_when_terminated:
-            msg = f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n{line.strip()}.'
-            self.unsend_line_notify_queue.put(self.gen_notify_format(msg))
+            msg = f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\nAs iperf sever, the client side has terminated session.'
+            self.validate_to_send_notify(msg)
         return
 
     def send_to_db_on_demend(self, record_time, mbps):
@@ -130,6 +131,11 @@ class Iperf3_logger(Amari_logger):
             self.zero_counter = 0
             return False
 
+    def check_if_is_in_parallel_mode(self, line):
+        if 'connected to' in line:
+            self.num_incoming_threads += 1
+        return
+
     def run_iperf3_session(self):
         '''
         The idea is to show original iperf3 server stdout, so not showing counter every second.
@@ -137,6 +143,7 @@ class Iperf3_logger(Amari_logger):
 
         print('==> Start iperf3 server session...\n')
         self.refresh_log_file()
+        self.num_incoming_threads = 0
         sleep(1)
 
         child = pexpect.spawnu(self.cmd, timeout=10,
@@ -156,14 +163,18 @@ class Iperf3_logger(Amari_logger):
             print(line)
             record_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
 
+            # count the incoming threads to determine parallel mode
+            self.check_if_is_in_parallel_mode(line)
+
             # detect session end and clean buffer
             if self.standby_pattern.match(line):
+                self.num_incoming_threads = 0
                 self.clean_buffer_and_send()
 
-            self.notify_when_client_termainated_on_demand(line)
+            self.check_if_client_is_terminated_and_send_notify_on_demand(line)
 
-            # get throughput but check first if parallel number > 2
-            if not self.is_in_parallel_mode:
+            # get throughput according to numbers of incoming thread
+            if self.num_incoming_threads < 2:
                 if not self.single_thread_mode_throughput_pattern.search(line):
                     continue
                 mbps = float(
@@ -198,8 +209,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--port', metavar='', default=5201, type=int,
                         help='iperf server port')
-    parser.add_argument('-P', '--parallel', action="store_true",
-                        help='detect client in parallel mode')
     parser.add_argument('-n', '--notify_when_terminated', action="store_true",
                         help='notify when terminated')
     parser.add_argument('-l', '--label', metavar='', default='', type=str,
@@ -215,7 +224,6 @@ if __name__ == '__main__':
 
     logger = Iperf3_logger(
         port=args.port,
-        parallel=args.parallel,
         notify_when_terminated=args.notify_when_terminated,
         label=args.label,
         project_field_name=args.project_field_name,
